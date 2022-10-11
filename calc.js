4acf90e5b56eff5b7a42f93c253764430cf899d0
syndesa <eric.rd@live.com>
2022-10-11 13:19:12 -0400

    Add responsive display for results and limit to 9 characters

--- calc.js
+++ calc.js
@@ -1,6 +1,17 @@
-results = document.querySelector('.results');
-numbers = document.querySelectorAll('.btn-dig');
+const results = document.querySelector('.results');
+const numbers = [...document.querySelectorAll('.btn-dig')];
+const ac = document.querySelector('#ac')
 
+numbers.forEach(number => {number.addEventListener('click', updateDisplay) });
+results.addEventListener('DOMSubtreeModified', () => results.textContent.length > 7 ? results.style.fontSize = '70px' : results.style.fontSize = '100px');
+// results.addEventListener('DOMSubtreeModified', displayResize);
+ac.addEventListener('click', () => results.textContent = '')
+
+
+// function displayResize(){
+//     const maxChars = 10;
+//     if(results.textContent.length > 3) results.textContent.replace(/.{2}/g, '$&,');
+// }
 
 function add(num1, num2){
     return num1 + num2
@@ -34,3 +45,20 @@ function operate(op, num1, num2){
     }
 
 
+function updateDisplay(){
+    if (results.textContent.length > 9) return;
+    if (results.textContent === '0'){
+        if (this.textContent === '0') {
+            return;
+        } else if (this.textContent === '.'){
+            return results.textContent += this.textContent;
+            
+        } else {
+            return results.textContent = this.textContent;
+        }
+    }  else {
+        return results.textContent = results.textContent + this.textContent;
+    }
+    console.log(this.textContent);
+    
+}
